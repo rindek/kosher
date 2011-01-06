1fa34fe8f0ddbb6f59b2f0fc0ccbee6799cd1e76
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-01-06 18:31:40 +0000

    Different can be different location.

--- lib/kosher/regexps.rb
+++ lib/kosher/regexps.rb
@@ -1,5 +1,5 @@
 module Kosher
-  DAMAGED     = "missing|torn|broken|split|different|discard|withdrawn|rent|stain|school|damaged|water"
+  DAMAGED     = "missing|torn|broken|split|discard|withdrawn|rent|stain|school|damaged|water"
   EXLIBRARY   = "(?:e?x|discarded|retired|former|has|have)[\\s._-]*lib"
   MARKED      = "(highlight|hilit|underlin)"
   MISSING_VOL = "(vols?|volume) only"
