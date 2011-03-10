d8aa7baf0f5dddf9ca4005f201c92182304a39e1
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-03-10 13:39:15 +0000

    Snapshot should know the venue URL

--- lib/kosher/snapshot.rb
+++ lib/kosher/snapshot.rb
@@ -1,4 +1,4 @@
 module Kosher
-  class Snapshot < Struct.new(:isbn, :asin, :sales_rank, :offers_count, :offers, :created_at)
+  class Snapshot < Struct.new(:venue_url, :isbn, :asin, :sales_rank, :offers_count, :offers, :created_at)
   end
 end
