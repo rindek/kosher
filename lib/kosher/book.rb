57600f096242a4c17c1d8251229d56561ab9e6ee
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-03-17 15:20:01 +0000

    Rename offers_count to offers_total

--- lib/kosher/book.rb
+++ lib/kosher/book.rb
@@ -1,5 +1,5 @@
 module Kosher
-  class Book < Struct.new(:venue, :isbn, :asin, :sales_rank, :offers_count, :offers, :created_at)
+  class Book < Struct.new(:venue, :isbn, :asin, :sales_rank, :offers_total, :offers, :created_at)
     def best_kosher_offer
       offer = offers.sort.first
 
