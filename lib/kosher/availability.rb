eec08f7a9496bb9565018d9b221fd7b81164ea07
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-05-27 19:46:36 +0100

    cull preorder field

--- lib/kosher/availability.rb
+++ lib/kosher/availability.rb
@@ -4,8 +4,15 @@ module Kosher
 
     self.threshold = 48
 
+    # Returns whether the availability is kosher.
+    #
+    # An availability is kosher if the item ships within a maximum threshold of
+    # hours.
+    #
+    # If we do not know when an item will ship (e.g. Amazon's preorders), we
+    # should set hours to nil, which will render the availability unkosher.
     def kosher?
-      hours <= threshold && !preorder
+      !hours.nil? && hours <= threshold
     end
   end
 end
