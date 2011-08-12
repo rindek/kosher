bfd597229df00fd2bf0ee25f5c6e1ad09753c415
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-08-12 19:03:58 +0100

    edit source formatting

--- lib/kosher.rb
+++ lib/kosher.rb
@@ -3,11 +3,11 @@ require 'money'
 require 'structure'
 
 module Kosher
-  autoload :Invalid,    'kosher/invalid'
-  autoload :Item,       'kosher/item'
-  autoload :Offer,      'kosher/offer'
-  autoload :Seller,     'kosher/seller'
-  autoload :Shipping,   'kosher/shipping'
-  autoload :Unit,       'kosher/unit'
-  autoload :Venue,      'kosher/venue'
+  autoload :Invalid,  'kosher/invalid'
+  autoload :Item,     'kosher/item'
+  autoload :Offer,    'kosher/offer'
+  autoload :Seller,   'kosher/seller'
+  autoload :Shipping, 'kosher/shipping'
+  autoload :Unit,     'kosher/unit'
+  autoload :Venue,    'kosher/venue'
 end
