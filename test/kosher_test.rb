abb4e3cf47d74ad83931938fd62f06f4856f9da3
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-09-01 11:46:44 +0100

    refactor test

--- test/kosher_test.rb
+++ test/kosher_test.rb
@@ -62,9 +62,6 @@ class TestKosher < MiniTest::Unit::TestCase
     assert_kind_of Venue, Venue.first
     assert 'Amazon.com', Venue.find(1).name
     assert_nil Venue.find(0)
-  end
-
-  def test_amazon_venues
     assert_equal 8, Venue.amazon.size
   end
 end
