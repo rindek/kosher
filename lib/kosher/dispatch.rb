4cd01e9faf0a9447668e014c62ceb1c8f1d0ea47
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-11-15 22:28:44 +0000

    Remove nil default

--- lib/kosher/dispatch.rb
+++ lib/kosher/dispatch.rb
@@ -3,7 +3,7 @@ module Kosher
     include Virtus
 
     attribute :cost, Price
-    attribute :domestic, Boolean, default: nil
+    attribute :domestic, Boolean
     attribute :fast, Boolean, default: false
     attribute :time, Integer, default: 2
   end
