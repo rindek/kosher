27d0104bc51cc3e7a104208ed494970e76b3eb0d
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-11-16 17:58:51 +0000

    Looks good

--- lib/kosher/dispatch.rb
+++ lib/kosher/dispatch.rb
@@ -2,9 +2,9 @@ module Kosher
   class Dispatch
     include Virtus
 
-    attribute :cost, Price
+    attribute :cost,     Price
     attribute :domestic, Boolean
-    attribute :fast, Boolean, default: false
-    attribute :time, Integer, default: 2
+    attribute :fast,     Boolean, default: false
+    attribute :time,     Integer, default: 2
   end
 end
